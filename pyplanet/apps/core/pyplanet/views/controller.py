e1466cf0bfb526548e272b232a90c557d8ac2142
15009199 <37383902+15009199@users.noreply.github.com>
2023-04-13 22:49:22 +0100

    Changed the key to show/hide some widgets from F8 and F9 to F10 and F11

--- pyplanet/apps/core/pyplanet/views/controller.py
+++ pyplanet/apps/core/pyplanet/views/controller.py
@@ -8,7 +8,7 @@ class ControllerView(TemplateView):
 		super().__init__(*args, **kwargs)
 		self.id = 'pyplanet__controller'
 
-		self.subscribe('f10', self.action_f8)
+		self.subscribe('f10', self.action_f10)
 
 	async def get_context_data(self):
 		from pyplanet.core import Controller
@@ -24,7 +24,7 @@ class ControllerView(TemplateView):
 	async def display(self, **kwargs):
 		return await super().display(**kwargs)
 
-	async def action_f8(self, player, *args, **kwargs):
+	async def action_f10(self, player, *args, **kwargs):
 		await self.manager.instance.chat(
 			'$ff0Toggling visibility. You can show/hide with f10, and show/hide when driving with f11', player
 		)
